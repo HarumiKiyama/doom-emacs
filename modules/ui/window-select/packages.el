261dbdd0314e6caa030aabbb1473075ea173bfef
harumi <h.kiyama0720@gmail.com>
2020-01-14 02:46:41 +0800

    clean window select

--- modules/ui/window-select/packages.el
+++ modules/ui/window-select/packages.el
@@ -1,9 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/window-select/packages.el
 
-(if (featurep! +switch-window)
-    (package! switch-window)
-  (package! ace-window))
-
-(when (featurep! +numbers)
-  (package! winum))
+(package! ace-window)
