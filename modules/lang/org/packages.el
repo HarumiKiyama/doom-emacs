c6feda74d7cb40db94dc9bfef5e6a49e0ef828dd
harumi <h.kiyama0720@gmail.com>
2020-01-13 23:29:53 +0800

    fix org packages

--- modules/lang/org/packages.el
+++ modules/lang/org/packages.el
@@ -37,55 +37,39 @@
 (package! org-cliplink)
 (package! org-bookmark-heading)
 
-(when (featurep! :editor evil +everywhere)
-  (package! evil-org :recipe (:host github :repo "hlissner/evil-org-mode")))
-(when (featurep! :tools pdf)
-  (package! org-pdfview))
-(when (featurep! :tools magit)
-  (package! orgit))
-(when (featurep! +brain)
-  (package! org-brain))
+(package! evil-org :recipe (:host github :repo "hlissner/evil-org-mode"))
+(package! org-pdfview)
+(package! orgit)
+(package! org-brain)
 (when (featurep! +dragndrop)
   (package! org-download))
 (when (featurep! +gnuplot)
   (package! gnuplot)
   (package! gnuplot-mode))
-(when (featurep! +ipython) ; DEPRECATED
-  (package! ob-ipython))
 (when (featurep! +jupyter)
   (package! jupyter))
-(when (featurep! +pomodoro)
-  (package! org-pomodoro))
-(when (featurep! +present)
-  (package! centered-window
-    :recipe (:host github :repo "anler/centered-window-mode"))
-  (package! org-tree-slide)
-  (package! org-re-reveal))
-(when (featurep! +journal)
-  (package! org-journal))
+(package! org-pomodoro)
+
+(package! centered-window
+  :recipe (:host github :repo "anler/centered-window-mode"))
+(package! org-tree-slide)
+(package! org-re-reveal)
+(package! org-journal)
 
 ;;; Babel
 (package! ob-async)
-(when (featurep! :lang crystal)
-  (package! ob-crystal))
-(when (featurep! :lang go)
-  (package! ob-go))
-(when (featurep! :lang nim)
-  (package! ob-nim))
-(when (featurep! :lang racket)
-  (package! ob-racket :recipe (:host github :repo "DEADB17/ob-racket")))
+(package! ob-go)
 (when (featurep! :lang rest)
   (package! ob-restclient))
-(when (featurep! :lang rust)
-  (package! ob-rust))
+(package! ob-rust)
+
 (when (featurep! :lang scala)
   (package! ob-ammonite))
 
 ;;; Export
-(when (featurep! +pandoc)
-  (package! ox-pandoc))
-(when (featurep! +hugo)
-  (package! ox-hugo
-    :recipe (:host github :repo "kaushalmodi/ox-hugo" :nonrecursive t)))
+(package! ox-pandoc)
+(package! ox-hugo
+  :recipe (:host github :repo "kaushalmodi/ox-hugo" :nonrecursive t))
+
 (when (featurep! :lang rst)
   (package! ox-rst))
