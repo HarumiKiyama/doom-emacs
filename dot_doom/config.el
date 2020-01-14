2ee38a58fb3ca38c7dbf4aa0156ad2367d8f71bd
harumi <h.kiyama0720@gmail.com>
2020-01-15 00:50:37 +0800

    remove org directory

--- dot_doom/config.el
+++ dot_doom/config.el
@@ -29,7 +29,6 @@
 
 
 ;; If you intend to use org, it is recommended you change this!
-(setq org-directory "~/org-mode")
 
 ;; If you want to change the style of line numbers, change this to `relative' or
 ;; `nil' to disable it:
