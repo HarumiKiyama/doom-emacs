c02368fbcbf278158e9af53d0b16a291bf14d046
harumi <h.kiyama0720@gmail.com>
2020-01-15 15:51:42 +0800

    remove doom

--- dot_doom/init.el
+++ dot_doom/init.el
@@ -3,14 +3,6 @@
 ;; This file controls what Doom modules are enabled and what order they load in.
 ;; Remember to run 'doom sync' after modifying it!
 
-;; NOTE Press 'SPC h d h' (or 'C-h d h' for non-vim users) to access Doom's
-;;      documentation. There you'll find information about all of Doom's modules
-;;      and what flags they support.
-
-;; NOTE Move your cursor over a module's name (or its flags) and press 'K' (or
-;;      'C-c g k' for non-vim users) to view its documentation. This works on
-;;      flags as well (those symbols that start with a plus).
-;;
 ;;      Alternatively, press 'gd' (or 'C-c g d') on a module to browse its
 ;;      directory (for easy access to its source code).
 
@@ -19,7 +11,6 @@
        ivy               ; a search engine for love and life
 
        :ui
-       doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        modeline          ; snazzy, Atom-inspired modeline, plus API
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
