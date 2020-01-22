bfdb5eab345e83e2f53004eb1cce427193aed200
harumi <h.kiyama0720@gmail.com>
2020-01-22 22:42:00 +0800

    add format line

--- modules/ui/dashboard/config.el
+++ modules/ui/dashboard/config.el
@@ -350,17 +350,16 @@ controlled by `+doom-dashboard-pwd-policy'."
   (let ((image (create-image (fancy-splash-image-file)))
         (point (point)))
     (add-text-properties
-         point (point) `(display ,image rear-nonsticky (display)))
-        (save-excursion
-          (goto-char point)
-          (insert (make-string
-                   (truncate
-                    (max 0 (+ 1 (/ (- +doom-dashboard--width
-                                      (car (image-size image nil)))
-                                   2))))
-                   ? ))
-          (insert-image image)
-          ))
+     point (point) `(display ,image rear-nonsticky (display)))
+    (save-excursion
+      (goto-char point)
+      (insert (make-string
+               (truncate
+                (max 0 (+ 1 (/ (- +doom-dashboard--width
+                                  (car (image-size image nil)))
+                               2))))
+               ? ))
+      (insert-image image)))
       (insert (make-string (or (cdr +doom-dashboard-banner-padding) 0)
                            ?\n)))
 
