63d3ef3bfb0d57a4b46d1d074790b15fb5925a9d
harumi <h.kiyama0720@gmail.com>
2020-02-03 00:08:09 +0800

    clean vc gutter config

--- modules/ui/vc-gutter/config.el
+++ modules/ui/vc-gutter/config.el
@@ -1,21 +1,7 @@
 ;;; ui/vc-gutter/config.el -*- lexical-binding: t; -*-
 
-(defvar +vc-gutter-in-margin t
-  "If non-nil, use the margin for diffs instead of the fringe.")
-
 (defvar +vc-gutter-in-remote-files nil
   "If non-nil, enable the vc gutter in remote files (e.g. open through TRAMP).")
-
-(defvar +vc-gutter-diff-unsaved-buffer t
-  "If non-nil, `diff-hl-flydiff-mode' will be activated. This allows on-the-fly
-diffing, even for unsaved buffers.")
-
-(defvar +vc-gutter-default-style t
-  "If non-nil, enable the default look of the vc gutter. This means subtle thin
-bitmaps on the left, an arrow bitmap for flycheck, and flycheck indicators moved
-to the right fringe.")
-
-
 ;;
 ;; Packages
 
@@ -92,20 +78,19 @@ is deferred until the file is saved. Respects `git-gutter:disabled-modes'."
 
 
 ;; subtle diff indicators in the fringe
-(when +vc-gutter-default-style
-  (after! git-gutter-fringe
-    ;; places the git gutter outside the margins.
-    (setq-default fringes-outside-margins t)
-    ;; thin fringe bitmaps
-    (define-fringe-bitmap 'git-gutter-fr:added [224]
-      nil nil '(center repeated))
-    (define-fringe-bitmap 'git-gutter-fr:modified [224]
-      nil nil '(center repeated))
-    (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240]
-      nil nil 'bottom)
-    ;; let diff have left fringe, flycheck can have right fringe
-    (after! flycheck
-      (setq flycheck-indication-mode 'right-fringe)
-      ;; A non-descript, left-pointing arrow
-      (define-fringe-bitmap 'flycheck-fringe-bitmap-double-arrow
-        [16 48 112 240 112 48 16] nil nil 'center))))
+(after! git-gutter-fringe
+  ;; places the git gutter outside the margins.
+  (setq-default fringes-outside-margins t)
+  ;; thin fringe bitmaps
+  (define-fringe-bitmap 'git-gutter-fr:added [224]
+    nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:modified [224]
+    nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240]
+    nil nil 'bottom)
+  ;; let diff have left fringe, flycheck can have right fringe
+  (after! flycheck
+    (setq flycheck-indication-mode 'right-fringe)
+    ;; A non-descript, left-pointing arrow
+    (define-fringe-bitmap 'flycheck-fringe-bitmap-double-arrow
+      [16 48 112 240 112 48 16] nil nil 'center)))
